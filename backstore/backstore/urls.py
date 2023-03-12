18d564494e07791b69980c7c872f56db372bb433
juliobarbagallo <julio.barbagallo@gmail.com>
2023-03-12 17:31:10 -0300

    feat: adds views, forms and templates for cart

--- backstore/backstore/urls.py
+++ backstore/backstore/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("products/", include("products.urls")),
     path("accounts/", include("accounts.urls")),
+    path("cart/", include("cart.urls")),
 ]
--- backstore/cart/views.py
+++ backstore/cart/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from products.models import Product
+from .models import Cart, CartItem
 
-# Create your views here.
+
+@login_required
+def add_to_cart(request, product_id):
+    product = Product.objects.get(id=product_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    if request.method == "POST":
+        quantity = int(request.POST["quantity"])
+        cart_item, item_created = CartItem.objects.get_or_create(
+            cart_item=cart, product=product
+        )
+        cart_item.quantity += quantity
+        cart_item.save()
+        return redirect("cart")
+
+    return render(request, "add_to_cart.html", {"product": product})
+
+
+@login_required
+def remove_from_cart(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    cart = Cart.objects.get(user=request.user)
+    cart.products.remove(product)
+    messages.success(request, f"{product.name} removed from cart.")
+    return redirect("cart:cart_detail")
+
+
+@login_required
+def clean_cart(request):
+    cart = Cart.objects.get(user=request.user)
+    cart.products.clear()
+    messages.success(request, "Cart cleaned.")
+    return redirect("cart:cart_detail")
+
+
+@login_required
+def cart_detail(request):
+    cart = Cart.objects.get(user=request.user)
+    cart_items = cart.products.all()
+    cart_total = cart.get_cart_total
+    return render(
+        request,
+        "cart/cart_detail.html",
+        {"cart_items": cart_items, "cart_total": cart_total},
+    )
