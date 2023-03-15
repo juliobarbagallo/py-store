25ac3f13f07d6406bcf2b0b67216d12aa5468448
juliobarbagallo <julio.barbagallo@gmail.com>
2023-03-15 12:45:49 -0300

    fix: add products to cart but need to fix cart details

--- backstore/cart/urls.py
+++ backstore/cart/urls.py
@@ -6,4 +6,7 @@ app_name = "cart"
 
 urlpatterns = [
     path("add/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("cart/", views.cart_detail, name="cart_detail"),
+    path("clean_cart", views.clean_cart, name="clean_cart"),
+    path("remove/<int:item_id>", views.remove_from_cart, name="remove_from_cart"),
 ]
--- backstore/cart/views.py
+++ backstore/cart/views.py
@@ -1,3 +1,6 @@
+import logging
+from django.urls import reverse
+from utils.logging_config import configure_logger
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -5,19 +8,36 @@ from products.models import Product
 from .models import Cart, CartItem
 
 
+app_name = "cart"
+log_file_path = f"{app_name}/{app_name}.out"
+log_level = logging.DEBUG
+logger = configure_logger(log_file_path, log_level)
+
+
 @login_required
 def add_to_cart(request, product_id):
+    logger.info("add_to_cart() was called")
     product = Product.objects.get(id=product_id)
+    logger.debug(f"{product=}")
     cart, created = Cart.objects.get_or_create(user=request.user)
+    logger.debug(f"{cart=}")
 
     if request.method == "POST":
         quantity = int(request.POST["quantity"])
-        cart_item, item_created = CartItem.objects.get_or_create(
-            cart_item=cart, product=product
-        )
+        try:
+            cart_item = CartItem.objects.get(cart=cart, product=product)
+            logger.debug(f"CartItem {cart_item.id} already exists")
+        except CartItem.DoesNotExist:
+            cart_item = CartItem.objects.create(
+                cart=cart, product=product, quantity=quantity
+            )
+            logger.debug(f"Creating new cart item")
+
         cart_item.quantity += quantity
         cart_item.save()
-        return redirect("cart")
+
+        logger.debug(f"Added {quantity} of {product.name} to cart")
+        return redirect(reverse("products:products"))
 
     return render(request, "add_to_cart.html", {"product": product})
 
@@ -41,11 +61,13 @@ def clean_cart(request):
 
 @login_required
 def cart_detail(request):
+    logging.info("cart_detail() was called.")
     cart = Cart.objects.get(user=request.user)
-    cart_items = cart.products.all()
+    cart_items = cart.get_cart_items
     cart_total = cart.get_cart_total
+    logging.debug(f"{cart=} - {cart_items=} - {cart_total=}")
     return render(
         request,
-        "cart/cart_detail.html",
+        "cart_detail.html",
         {"cart_items": cart_items, "cart_total": cart_total},
     )
