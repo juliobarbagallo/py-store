ae36ed0c56e6014ba4c244ae3dc5ae91a7733060
juliobarbagallo <julio.barbagallo@gmail.com>
2023-03-18 19:06:21 -0300

    feat: add redis for caching

--- backstore/products/views.py
+++ backstore/products/views.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from utils.logging_config import configure_logger
 from django.shortcuts import render, redirect
 from .forms import ProductForm
@@ -11,6 +12,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 
+from django.core.cache import cache
+import json
+import redis
+from django.conf import settings
+
 app_name = "products"
 log_file_path = f"{app_name}/{app_name}.out"
 log_level = logging.DEBUG
@@ -22,11 +28,50 @@ class Ping(APIView):
         return Response({"message": "pong"})
 
 
+# class ProductList(APIView):
+#     def get(self, request, format=None):
+
+#         r = redis.Redis(host=os.getenv("REDIS_HOST"), port=os.getenv("REDIS_PORT"), db=os.getenv("REDIS_DB"))
+#         product_list = r.get('product_list')
+#         if product_list:
+#             # If the product list is in Redis, return it
+#             products = json.loads(product_list)
+#         else:
+#             # If the product list is not in Redis, query the database and cache the result
+#             products = Product.objects.all()
+#             serializer = ProductSerializer(products, many=True)
+#             products = serializer.data
+#             r.set('product_list', json.dumps(products))
+
+#         return Response(products)
+
+
 class ProductList(APIView):
     def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+        try:
+            r = redis.Redis(
+                host=os.getenv("REDIS_HOST"),
+                port=os.getenv("REDIS_PORT"),
+                db=os.getenv("REDIS_DB"),
+            )
+            product_list = r.get("product_list")
+            if product_list:
+                logger.debug("Products are on redis.")
+                products = json.loads(product_list)
+            else:
+                logger.debug("Products are not on redis.")
+                products = Product.objects.all()
+                serializer = ProductSerializer(products, many=True)
+                products = serializer.data
+                r.set("product_list", json.dumps(products))
+        except redis.ConnectionError as e:
+            # If there was an error connecting to Redis, log a warning and fallback to the database
+            logging.warning(f"Redis connection error: {e}")
+            products = Product.objects.all()
+            serializer = ProductSerializer(products, many=True)
+            products = serializer.data
+
+        return Response(products)
 
 
 @login_required
